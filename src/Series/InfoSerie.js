9f1b9ea74404524347afdc966c7ad39736f124cb
AlbinoFreitas <albino_freitas@hotmail.com>
2019-08-04 17:35:12 -0300

    Header da serie em outro componente

--- src/Series/InfoSerie.js
+++ src/Series/InfoSerie.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { Badge } from 'reactstrap';
 import axios from 'axios';
+
+import HeaderInfoSerie from './HeaderInfoSerie';
 
 const status = {
   'PARA_ASSISTIR': 'Para Assistir',
@@ -56,42 +57,9 @@ function InfoSerie({ match }) {
       .then(res => setMode('INFO'))
   }
 
-  const headerStyle = {
-    height: '50vh',
-    minHeight: '500px',
-    backgroundImage: `url('${data.background}')`,
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    backgroundRepeat: 'no-repeat'
-  };
-
   return (
     <div>
-      <header style={headerStyle}>
-        <div className='h-100' style={{ backgroundColor: 'rgba(0,0,0,0.7)'}}>
-          <div className='h-100 container'>
-            <div className='row h-100 align-items-center'>
-              <div className='col-3'>
-                  <img className='img-fluid img-thumbnail' src={data.poster} alt={data.name} />
-              </div>
-              <div className='col-8'>
-                <h1 className='text-weight-light text-white'>{data.name}</h1>
-                <div className='lead text-white'>
-                  { data.status === 'PARA_ASSISTIR' && <Badge color='success'>Para Assistir</Badge> }
-                  { data.status === 'ASSISTINDO' && <Badge color='warning'>Assistindo</Badge> }
-                  { data.status === 'ASSISTIDO' && <Badge color='danger'>Assistido</Badge> }
-                  <p>Gênero: {data.genre}</p>
-                </div>
-                {
-                  mode !== 'EDITING' &&
-                  <button className='btn btn-primary btn-sm' onClick={() => setMode('EDITING')}>Editar Série</button>
-                }
-              </div>
-            </div>
-          </div>
-          
-        </div>
-      </header>
+      <HeaderInfoSerie data={data} mode={mode} setMode={setMode}></HeaderInfoSerie>
 
       <div className='container'>
         {
